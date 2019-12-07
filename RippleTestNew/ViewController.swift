650bd48fc8fc9112a0c7e05db6ce47415e9e4f67
Alex Moreno <alx.spain@gmail.com>
2019-12-07 19:11:21 +0000

    Adding Ripple Effect

--- RippleTestNew/ViewController.swift
+++ RippleTestNew/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var select = false
+    @IBOutlet weak var selector: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-
+    @IBAction func plusButtonPressed(_ sender: UIButton) {
+        let position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 2)
+        if select {
+            view.rippleFill(location: position, color: UIColor.black)
+        } else {
+            view.rippleBorder(location: position, color: UIColor.black)
+        }
+    }
+    
+    @IBAction func onSwitchChanged(_ sender: UISwitch) {
+        if sender.isOn {
+            select = true
+        } else {
+            select = false
+        }
+    }
 }
 
