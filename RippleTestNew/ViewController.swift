d7fa5d748117aebc1b58fe7e6966f93c416963b8
Alex Moreno <alx.spain@gmail.com>
2019-12-07 18:28:52 +0000

    Initial Commit

---
+++ RippleTestNew/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RippleTestNew
+//
+//  Created by Alexander Moreno Guillén on 7/12/19.
+//  Copyright © 2019 Alexander Moreno Guillén. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
